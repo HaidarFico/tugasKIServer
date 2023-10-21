bb0f8c7e138f45781572c9970e972bedf837523e
HaidarFico <haidarficoi@gmail.com>
2023-10-21 21:52:03 +0700

    feat: added forms

---
+++ form.py
@@ -0,0 +1,23 @@
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_wtf import FlaskForm
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "password"})
+    submit = SubmitField('Register')
+    
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "password"})
+    submit = SubmitField('Login')
+
+class FileUploadForm(FlaskForm):
+    filename = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "filename"})
+    file = FileField(validators=[InputRequired()], name='file')
+    submit = SubmitField('upload')
+    
+class PrivateDataForm(FlaskForm):
+    data_name = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "data_name"})
+    data = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "data"})
+    submit = SubmitField('Login')
